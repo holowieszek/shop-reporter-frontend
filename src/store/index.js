bfaafa2d8a3eb3d30826f526400266cf13e1c313
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-18 18:40:43 +0200

    Added store module for perfume

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import User from './modules/user'
+import Perfume from './modules/perfume'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    User
+    User,
+    Perfume
   }
 })
 
