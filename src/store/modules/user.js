8dd01a39acf6a620298b24eb095e492c77cc1e2a
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-18 13:08:06 +0200

    Added user vuex module

---
+++ src/store/modules/user.js
@@ -0,0 +1,32 @@
+import asyncWrapper from '../../utils/asyncWrapper'
+import { signUp } from '../../services/user.service'
+import { SET_USER } from '../mutationTypes'
+
+const state = {
+  user: []
+}
+
+const getters = {
+  user: state => state.user
+}
+
+const actions = {
+  async signUp({ commit }, data) {
+    const { error, result } = await asyncWrapper(signUp(data))
+
+    return !error ? commit(SET_USER, result.user) : error;
+  }
+}
+
+const mutations = {
+  [SET_USER] (state, payload) {
+    state.user = payload
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
