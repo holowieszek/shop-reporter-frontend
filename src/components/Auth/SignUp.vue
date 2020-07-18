e7ca998d7b7c43e6ac1401287f16a3ad1c3f1a8b
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-18 11:29:07 +0200

    Designing signIn/signUp components

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,9 +1,69 @@
 <template>
-  <h1>signup</h1>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Sign up</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Email"
+                name="emaik"
+                prepend-icon="mdi-account"
+                type="text"
+                v-model="email"
+              ></v-text-field>
+
+              <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="password"
+              ></v-text-field>
+
+              <v-text-field
+                id="confirm-password"
+                label="Confirm password"
+                name="confirm-password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="confirmPassword"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="onSubmit">Sign up</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  }),
+  methods: {
+    onSubmit() {
+      const { email, password, confirmPassword } = this
+      if (password !== confirmPassword) {
+        return;
+      }
+
+      console.log({ email, password, confirmPassword });
+    }
+  }
+};
 </script>
 
 <style>
