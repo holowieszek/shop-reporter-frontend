dcedcfa32b3b8e07f576e9ea8ddad3bbb0d0d35a
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-18 12:55:21 +0200

    Added Amplify configuration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify from 'aws-amplify'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: process.env.VUE_APP_REGION,
+    userPoolId: process.env.VUE_APP_USER_POOL_ID,
+    identityPoolId: process.env.VUE_APP_IDENTITY_POOL_ID,
+    userPoolWebClientId: process.env.VUE_APP_APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'perfume',
+        endpoint: process.env.VUE_APP_API_URL,
+        region: process.env.VUE_APP_API_REGION
+      }
+    ]
+  }
+});
 
 new Vue({
   vuetify,
